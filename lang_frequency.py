c3fbc6efa4dd4fb6638ff2a5ea666fc92b9771ff
yuriy_beloff <paganism@mail.ru>
2017-10-22 22:09:44 +0300

    исправление

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,21 +1,24 @@
 from collections import Counter
 import sys
 
-filepath = sys.argv[1]
-count_to_output = 10
 
 def load_data(filepath):
     with open(filepath, 'r') as f:
-        raw_data = f.read().split()
+        raw_data = f.read()
     return raw_data
 
 
 def get_most_frequent_words(text):
+    count_to_output = 10
+    splitted_text = text.split()
     clean_data = []
     for word in text:
-        clean_data.append(word.strip(',.!-'))
-    return Counter(text).most_common(count_to_output)
+        clean_data.append(word.strip(',.!-[]()*/{}+=<>?&^$#@~`;:'))
+    return Counter(splitted_text).most_common(count_to_output)
 
 
 if __name__ == '__main__':
-    print(get_most_frequent_words(load_data("textfile.txt")))
+    filepath = sys.argv[1]
+    for i in get_most_frequent_words(load_data(filepath)):
+        print("слово: ",i,"количество повторений: ")
+    #print(get_most_frequent_words(load_data(filepath))[0][0])
